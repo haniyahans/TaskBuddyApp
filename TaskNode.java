38b12b49b0376425eebc26d9e251d4315f3317d8
Sabrina150506 <azahrasabrina03@gmail.com>
2025-06-20 19:06:01 +0700

    Update TaskNode.java

--- TaskNode.java
+++ TaskNode.java
@@ -1,15 +1,15 @@
 import java.util.LinkedList;
-public class TaskNode {//untuk masukin tugas ke tree (punya main tugas dan sub tugas)
-    Task task; //simpan objek tugas dan informasinya, jadi bikin objek tugasnya dulu (nama, prioritas, dl) baru dimasukin ke tree
-    LinkedList<TaskNode> subtasks;//biar punya sub tugas dan jadi tree
-    public TaskNode(Task task) {//konstruktor, disini dia buat tugas utamanya dulu, trs baru tambahin sub tugasnya pake add
+public class TaskNode {//untuk masukin tugas ke tree (punya tugas utama dan sub tugas)
+    Task task; //simpan objek tugas dan informasinya, jadi bikin objek tugasnya dulu (nama, prioritas, dll) baru dimasukin ke tree
+    LinkedList<TaskNode> subtasks;//agar mempunyai sub tugas dan jadi tree
+    public TaskNode(Task task) {//konstruktor, disini bisa buat tugas utamanya dulu, lalu baru tambahin sub tugasnya pake add
         this.task = task;//tambah tugas utama
         this.subtasks = new LinkedList<>();//tambah subtugas
     }
-    public Task getTask() {//untuk nampilin root task
+    public Task getTask() {//untuk ambil objek task
         return task;
     }
-    public LinkedList<TaskNode> getSubtasks() {//untuk nampilin subtask
+    public LinkedList<TaskNode> getSubtasks() {//untuk ambil subtask
         return subtasks;
     }
 }
