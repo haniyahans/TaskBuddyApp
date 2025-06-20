7bffd177bdb2839ca3b6fdee6c44f0763e140d94
Sabrina150506 <azahrasabrina03@gmail.com>
2025-06-20 18:46:18 +0700

    Update SimpleTask.java

--- SimpleTask.java
+++ SimpleTask.java
@@ -1,11 +1,11 @@
 import java.time.LocalDate;
-public class SimpleTask extends Task {//penerapan inheritance dari oop (untuk bikin deskripsi tugas)
-    public SimpleTask(String name, String description, int priority, LocalDate deadline) {//dia terapin polymorphism, jadi bikin objeknya pake ---Task tugas = new SimpleTask(...)---
+public class SimpleTask extends Task {//penerapan inheritance dari oop (untuk bikin deskripsi tugas & objek)
+    public SimpleTask(String name, String description, int priority, LocalDate deadline) {//dia bisa nerapin polymorphism, jadi bikin objeknya pake ---Task tugas = new SimpleTask(...)---
         super(name, description, priority, deadline); //turunan task, isCompletednya ikut default
     }
 
     @Override
-    public String getTaskInfo() {//print info tugasnya
+    public String getTaskInfo() {//print informasi tugasnya
         return String.format("%-15s | %-20s | Priority: %-2d | Deadline: %-12s | Done: %-5b",
                              "- " + name,
                              description,
